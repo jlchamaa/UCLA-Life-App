b88cd4d5959aeb4cd9761f15ecf136af9ff8e0e3
Jean-Luc Chamaa <jlchamaa@gmail.com>
2013-11-02 17:23:59 -0700

    Two buttons & Rotation

--- Project Files/gen/com/ALCU/angel/R.java
+++ Project Files/gen/com/ALCU/angel/R.java
@@ -21,13 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int bluebutton9=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int campus1=0x7f020000;
+        public static final int campus2=0x7f020001;
+        public static final int dining_pressed=0x7f020002;
+        public static final int dining_unpressed=0x7f020003;
+        public static final int housing_pressed=0x7f020004;
+        public static final int housing_unpressed=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int intial_button=0x7f020007;
+        public static final int intial_button_housing=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
-        public static final int diningbutton=0x7f080000;
-        public static final int textView1=0x7f080001;
+        public static final int button1=0x7f080000;
+        public static final int button2=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- Project Files/src/com/ALCU/angel/MainActivity.java
+++ Project Files/src/com/ALCU/angel/MainActivity.java
@@ -2,15 +2,34 @@ package com.ALCU.angel;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.*;
+import android.widget.RelativeLayout;
+import android.graphics.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		OrientationEventListener spincheck = new OrientationEventListener(this) 
+		{
+			
+			@Override
+			public void onOrientationChanged(int orientation)
+			{
+				RelativeLayout rl= (RelativeLayout)findViewById(R.layout.activity_main);
+				if((orientation>315 && orientation<370)||(orientation>-1 &&orientation<45))
+					rl.setBackgroundResource(R.drawable.campus1);
+				else
+					rl.setBackgroundResource(R.drawable.campus2);
+			}
+		};
+	};
+		
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,4 +38,5 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
 }
